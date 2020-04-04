3cbca87047707ab7e4b6b48f39daac23d07e7ade
Muralidharan V <muralidharan1307@gmail.com>
2020-04-04 20:27:25 +0530

    Push Notification
    Signed-off-by: Muralidharan V <muralidharan1307@gmail.com>

---
+++ app/src/main/java/com/example/myapplication/PushNofitication/CustomMessagingService.java
@@ -0,0 +1,92 @@
+package com.example.myapplication.PushNofitication;
+
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.example.myapplication.MainActivity;
+import com.example.myapplication.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class CustomMessagingService extends FirebaseMessagingService {
+
+    NotificationManager notificationManager;
+    Notification notification;
+
+    Uri defaultSoundUri;
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (remoteMessage != null) {
+
+            //message without data payload
+           /* String title = remoteMessage.getNotification().getTitle();
+            String message = remoteMessage.getNotification().getBody();
+            notifyUser(title, message);
+*/
+            // Check if message contains a data payload.
+            if (remoteMessage.getData().size() > 0) {
+
+                String title = remoteMessage.getData().get("title");
+                String message = remoteMessage.getData().get("message");
+                notifyUser(title, message);
+
+            }
+
+        }
+
+    }
+
+    private void notifyUser(String title, String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = getString(R.string.default_notification_channel_id);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.notification_logo)
+                        .setContentTitle(title)
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setColor(Color.BLUE)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // For android Oreo and above  notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Fcm notifications",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0, notificationBuilder.build());
+
+    }
+
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+    }
+}
